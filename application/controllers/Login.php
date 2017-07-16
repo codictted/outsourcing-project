a5679f7b007dd700d567a309e0083e183fbde9d4
Arlene Mariano <Arlene Mariano>
2017-07-16 08:06:45 +0800

    Remove some sensitive information

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -43,32 +43,6 @@
             redirect(base_url());
         }
 
-        public function captcha() {
-            $this->load->helper('captcha');
-            $vals = array(
-            'word'          => 'Random word',
-            'img_path'      => './captcha/',
-            'img_url'       => base_url().'assets/captcha/',
-            'img_width'     => '150',
-            'img_height'    => 30,
-            'expiration'    => 7200,
-            'word_length'   => 8,
-            'font_size'     => 16,
-            'img_id'        => 'Imageid',
-            'pool'          => '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-
-            // White background and border, black text and red grid
-            'colors'        => array(
-                    'background' => array(255, 255, 255),
-                    'border' => array(255, 255, 255),
-                    'text' => array(0, 0, 0),
-                    'grid' => array(255, 40, 40)
-                )
-            );
-
-            $cap = create_captcha($vals);
-            print_r($cap['image']);
-            echo $cap['image'];
-        }
+        
 	}
 ?>

22111297c88da645ffa4a5e4479187740bd30d81
leewolf-karl <leewolf07@gmail.com>
2017-08-21 09:38:26 +0800

    Adding of maintenance

--- application/views/admin-header.php
+++ application/views/admin-header.php
@@ -10,10 +10,10 @@
         <script src="<?php echo base_url(); ?>assets/js/dataTables.min.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/jquery-function.js"></script>
         <script src="<?php echo base_url(); ?>assets/magicsuggest/magicsuggest-min.js"></script>
-        <script src="<?php echo base_url(); ?>assets/select2-4.0.3/dist/js/select2.min.js"></script>    
+        <script src="<?php echo base_url(); ?>assets/select2/dist/js/select2.min.js"></script>    
 
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/select2-4.0.3/dist/css/select2.min.css">   
-       <!--  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/magicsuggest/magdisicsuggest.css"> -->
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/select2/dist/css/select2.min.css">   
+    
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/sb-admin.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/dataTables.min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootswatch.css">
